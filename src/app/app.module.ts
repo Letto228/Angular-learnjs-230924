306ad91dd1bd0ad4e525413aad1c31d68f881f47
AleAnaKirillov <aleanakirillov@Greenatom.ru>
2024-09-27 22:50:45 +0300

    feat: added eslint and prettier configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import test from '@test';
-
-window.user;
 
 @NgModule({
     declarations: [AppComponent],
