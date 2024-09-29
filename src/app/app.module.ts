6a2ffd6c73ab2b6ca0597cd0531c49f063677152
Антон Голубев <golubev_ak@crc.ru>
2024-09-29 18:26:06 +0400

    feat: 'added config for prettierrc andd eslint'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import test from '@test';
-
-window.user;
 
 @NgModule({
     declarations: [AppComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
-    bootstrap: [AppComponent],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
