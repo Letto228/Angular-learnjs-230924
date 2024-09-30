d541b6c4bcaf29fdc1cd6bc590b31bb24c7187b3
smirnov_alexx <smirnov1991alexx@gmail.com>
2024-09-30 19:59:09 +0500

    lecture 1 homework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import test from '@test';
+// import test from '@test';
 
 window.user;
 
