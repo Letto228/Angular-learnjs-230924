99fc6d9db2e5109cdeb9549d64b9b124ff58465f
pakhomova <pakhomova@kraud.ru>
2024-09-30 19:57:30 +0300

    changes for eslint and prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import test from '@test';
+//import test from '@test';
 
 window.user;
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
-    providers: [],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
