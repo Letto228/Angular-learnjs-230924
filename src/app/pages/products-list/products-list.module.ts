2a56d59f24cc31775bac504f1034de721ee9906a
zloy_monk <phamnhuquockhanh@yandex.ru>
2024-10-07 21:31:31 +0700

    feat: add card component

--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,8 +1,15 @@
 import {NgModule} from '@angular/core';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+
 import {ProductsListComponent} from './products-list.component';
+import {CardComponent} from './card/card.component';
 
 @NgModule({
-    declarations: [ProductsListComponent],
-    exports: [ProductsListComponent],
+    declarations: [ProductsListComponent, CardComponent],
+    exports: [ProductsListComponent, CardComponent],
+    imports: [MatButtonModule, MatCardModule, MatIconModule],
 })
 export class ProductsListModule {}
