4731bd00a79464840acb3399b7f601b070798d12
Anastasia Brushkevich <perfishina@gmail.com>
2024-10-10 12:50:12 +0300

    added card component

---
+++ src/app/pages/products-list/card/card.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+import {Product} from 'src/app/shared/products/product.interface';
+
+@Component({
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css'],
+})
+export class CardComponent {
+    @Input() product?: Product | undefined;
+}
