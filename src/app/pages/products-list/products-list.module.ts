06e9fb8871044ec4c70f4cf1971e31c9cc7c3612
Irina Vorobeva <i.s.vorobjeva@yandex.ru>
2024-10-13 15:12:23 +0500

    added product card

--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,8 +1,12 @@
 import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import {ProductsListComponent} from './products-list.component';
+import {CardComponent} from './card/card.component';
 
 @NgModule({
-    declarations: [ProductsListComponent],
-    exports: [ProductsListComponent],
+    imports: [MatCardModule, MatButtonModule],
+    declarations: [ProductsListComponent, CardComponent],
+    exports: [ProductsListComponent, CardComponent],
 })
 export class ProductsListModule {}
