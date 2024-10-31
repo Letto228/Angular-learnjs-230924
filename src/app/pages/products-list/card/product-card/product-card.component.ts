3e689d490aa3672d82d47f01cfa8630b610853c7
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2024-11-01 01:46:26 +0300

    feat: implement 'product-card' component, render 5 products

--- src/app/pages/products-list/card/product-card/product-card.component.ts
+++ src/app/pages/products-list/card/product-card/product-card.component.ts
@@ -1,8 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {Product} from '../../../../shared/products/product.interface';
 
 @Component({
     selector: 'app-product-card',
     templateUrl: './product-card.component.html',
     styleUrls: ['./product-card.component.css'],
+    providers: [MatSnackBar],
 })
-export class ProductCardComponent {}
+export class ProductCardComponent {
+    @Input() product: Product | null = null;
+    // eslint-disable-next-line @typescript-eslint/member-ordering
+    private readonly snackBar = inject(MatSnackBar);
+
+    buyProduct(productName: string | undefined): void {
+        this.snackBar.open(
+            productName
+                ? `"${productName}" добавлен в корзину`
+                : 'Ошибка добавления товара в корзину',
+            productName ? 'Звучит как сказка!' : 'жестб...',
+        );
+    }
+}
