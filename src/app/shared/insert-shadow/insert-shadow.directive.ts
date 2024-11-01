45f841c3ba9798569e01b96a18b9c0a4907ebfcd
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-11-01 07:37:33 +0300

    chore: sanitize project

--- src/app/shared/insert-shadow/insert-shadow.directive.ts
+++ src/app/shared/insert-shadow/insert-shadow.directive.ts
@@ -4,32 +4,6 @@ import {Directive, HostBinding, HostListener} from '@angular/core';
     selector: '[appInsertShadow]',
 })
 export class InsertShadowDirective {
-    // private readonly element = inject(ElementRef).nativeElement;
-    // private readonly render2 = inject(Renderer2);
-
-    // <host-element (click)="InsertShadowDirective.onClick($event.target)"></host-element>
-    // @HostListener('click', ['$event.target'])
-    // onClick(target: Event['target']) {
-    //     console.log('Host clicked', target);
-    // }
-
-    // <host-element (click)="InsertShadowDirective.onClick($event.clientX, $event.clientY)"></host-element>
-    // @HostListener('click', ['$event.clientX', '$event.clientY'])
-    // onClick(x: number, y: number) {
-    //     console.log('Host clicked', x, y, this.element);
-    // }
-
-    // <host-element [style.boxShadow]="InsertShadowDirective.boxShadow"></host-element>
-    // @HostBinding('style.boxShadow')
-    // boxShadow = '';
-
-    // @HostListener('click')
-    // toggleShadow() {
-    //     this.boxShadow = !this.boxShadow ? 'inset 0 0 10px #000' : '';
-    // }
-
-    // -----------------------------------------------------------------
-
     private isBoxShadowActive = false;
 
     @HostBinding('style.boxShadow')
