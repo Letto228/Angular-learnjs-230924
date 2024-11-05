fedae5b9390e6232bf239f564feddd452665730c
Vasilii Ryndin <vryndin@pminst.ru>
2024-11-05 17:22:37 +0300

    Реализация пагинации

--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,13 +1,24 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {PaginationModule} from 'src/app/shared/pagination/pagination.module';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 import {ProductsListComponent} from './products-list.component';
 import {CardModule} from './card/card.module';
 import {MyNgIfModule} from '../../shared/my-ng-if/my-ng-if.module';
 
 @NgModule({
     declarations: [ProductsListComponent],
-    imports: [CommonModule, CardModule, MyNgIfModule, MatProgressSpinnerModule],
+    imports: [
+        CommonModule,
+        CardModule,
+        MyNgIfModule,
+        MatProgressSpinnerModule,
+        MatButtonModule,
+        MatIconModule,
+        PaginationModule,
+    ],
     exports: [ProductsListComponent],
 })
 export class ProductsListModule {}
---
+++ src/app/shared/pagination/pagination.directive.ts
@@ -0,0 +1,111 @@
+import {
+    Directive,
+    inject,
+    Input,
+    OnChanges,
+    SimpleChanges,
+    TemplateRef,
+    ViewContainerRef,
+} from '@angular/core';
+import {BehaviorSubject, filter, map} from 'rxjs';
+
+interface PaginationDirectiveContext<T> {
+    $implicit: T[];
+    index: number;
+    pageIndexes: number[];
+    appPaginationOf: T[];
+    next: () => void;
+    back: () => void;
+    selectIndex: (pageIndex: number) => void;
+}
+
+@Directive({
+    selector: '[appPagination]',
+})
+export class PaginationDirective<T> implements OnChanges {
+    private readonly viewContainerRef = inject(ViewContainerRef);
+    private readonly templateRef = inject<TemplateRef<PaginationDirectiveContext<T>>>(TemplateRef);
+
+    private readonly currentIndex$ = new BehaviorSubject<number>(0);
+
+    @Input() appPaginationOf: T[] | null | undefined;
+
+    @Input() appPaginationChunkSize = 1;
+
+    constructor() {
+        this.listenCurrentIndex();
+    }
+
+    ngOnChanges({appPaginationOf, appPaginationChunkSize}: SimpleChanges): void {
+        if (appPaginationOf || appPaginationChunkSize) {
+            this.updateView();
+        }
+    }
+
+    private get shouldShowView(): boolean {
+        return !!this.appPaginationOf?.length;
+    }
+
+    private get pages(): number[] {
+        if (this.appPaginationOf?.length && this.appPaginationChunkSize) {
+            const pagesCount = Math.ceil(this.appPaginationOf.length / this.appPaginationChunkSize);
+
+            return [...new Array(pagesCount).keys()];
+        }
+
+        return [];
+    }
+
+    private updateView() {
+        if (this.shouldShowView) {
+            this.currentIndex$.next(0);
+
+            return;
+        }
+
+        this.viewContainerRef.clear();
+    }
+
+    private listenCurrentIndex() {
+        this.currentIndex$
+            .pipe(
+                filter(() => this.shouldShowView),
+                map(currentIndex => this.getCurrentContext(currentIndex)),
+            )
+            .subscribe(context => {
+                this.viewContainerRef.clear();
+                this.viewContainerRef.createEmbeddedView(this.templateRef, context);
+            });
+    }
+
+    private getCurrentContext(currentIndex: number): PaginationDirectiveContext<T> {
+        const appPaginationOf = Array.isArray(this.appPaginationOf) ? this.appPaginationOf : [];
+
+        const currentItems = appPaginationOf.slice(
+            currentIndex * this.appPaginationChunkSize,
+            currentIndex * this.appPaginationChunkSize + this.appPaginationChunkSize,
+        );
+
+        return {
+            appPaginationOf,
+            index: currentIndex,
+            $implicit: currentItems,
+            next: this.next.bind(this),
+            pageIndexes: this.pages,
+            back: this.back.bind(this),
+            selectIndex: this.selectIndex.bind(this),
+        };
+    }
+
+    private next() {
+        this.currentIndex$.next(this.currentIndex$.value + 1);
+    }
+
+    private back() {
+        this.currentIndex$.next(this.currentIndex$.value - 1);
+    }
+
+    private selectIndex(pageIndex: number) {
+        this.currentIndex$.next(pageIndex);
+    }
+}
