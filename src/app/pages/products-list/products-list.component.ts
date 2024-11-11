ec04f9969079654ed071885d985d314f4e10b3fe
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-11-11 23:09:56 +0300

    feat: add categories navigation

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {map, switchMap, tap} from 'rxjs';
 import {Product} from '../../shared/products/product.interface';
 import {ProductsStoreService} from '../../shared/products/products-store.service';
 
@@ -11,7 +13,17 @@ import {ProductsStoreService} from '../../shared/products/products-store.service
 export class ProductsListComponent {
     private readonly productsStoreService = inject(ProductsStoreService);
 
-    readonly products$ = this.productsStoreService.products$;
+    // readonly products$ = this.productsStoreService.products$;
+
+    private readonly activatedRoute = inject(ActivatedRoute);
+
+    readonly products$ = this.activatedRoute.paramMap.pipe(
+        map(paramMap => paramMap.get('subCategoryId')),
+        tap(subCategoryId => {
+            this.productsStoreService.loadProducts(subCategoryId);
+        }),
+        switchMap(() => this.productsStoreService.products$),
+    );
 
     constructor() {
         this.productsStoreService.loadProducts();
