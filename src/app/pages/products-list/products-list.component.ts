3981ff4c1c23527eb6d3af8d3e6aedd1ebcb437a
Vasilii Ryndin <vryndin@pminst.ru>
2024-11-20 18:20:35 +0300

    HW10 список товаров по категориям

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {map, switchMap, tap} from 'rxjs/operators';
 import {Product} from '../../shared/products/product.interface';
 import {ProductsStoreService} from '../../shared/products/products-store.service';
 
@@ -10,12 +12,14 @@ import {ProductsStoreService} from '../../shared/products/products-store.service
 })
 export class ProductsListComponent {
     private readonly productsStoreService = inject(ProductsStoreService);
-
-    readonly products$ = this.productsStoreService.products$;
-
-    constructor() {
-        this.productsStoreService.loadProducts();
-    }
+    private readonly activatedRoute = inject(ActivatedRoute);
+    readonly products$ = this.activatedRoute.paramMap.pipe(
+        map(paramMap => paramMap.get('category')),
+        tap(categoryId => {
+            this.productsStoreService.loadProducts(categoryId);
+        }),
+        switchMap(() => this.productsStoreService.products$),
+    );
 
     trackByProductId(_index: number, item: Product) {
         return item._id;
